2f4b51f5892571c162307e1c59607b6552384625
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2023-02-27 12:39:36 +0600

    service route added

--- index.js
+++ index.js
@@ -14,7 +14,29 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
         try{
+            const serviceCollection = client.db('alexis').collection('services');
 
+            app.get('/services', async(req,res)=>{
+                const query ={};
+                const cursor = serviceCollection.find(query);
+                const services = await cursor.limit(3).toArray();
+                res.send(services)
+            });
+    
+            app.get('/allServices', async(req,res)=>{
+                const query ={};
+                const cursor = serviceCollection.find(query);
+                const service = await cursor.toArray();
+                res.send(service)
+            });
+    
+            app.get('/services/:id',async(req,res)=>{
+                const id = req.params.id;
+                const query = {_id:new ObjectId(id)};
+                const services = await serviceCollection.findOne(query);
+                res.send(services);
+    
+            });
         }
         finally{
             
